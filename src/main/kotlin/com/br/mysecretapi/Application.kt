cdbc0b3223b276904148aeae057cf83a33fef96c
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-09-07 00:41:49 -0300

    main class name refactor

--- src/main/kotlin/com/br/mysecretapi/Application.kt
+++ src/main/kotlin/com/br/mysecretapi/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MySecretApiApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<MySecretApiApplication>(*args)
+	runApplication<Application>(*args)
 }
