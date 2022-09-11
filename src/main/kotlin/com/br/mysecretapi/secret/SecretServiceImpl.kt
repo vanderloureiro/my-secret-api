f5ff47f2d9126d0e8f2c431c22ea23b7a9b2e095
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-09-11 18:18:48 -0300

    fix service anotation in SecretServiceImpl

--- src/main/kotlin/com/br/mysecretapi/secret/SecretServiceImpl.kt
+++ src/main/kotlin/com/br/mysecretapi/secret/SecretServiceImpl.kt
@@ -1,6 +1,20 @@
 package com.br.mysecretapi.secret
 
 import com.br.mysecretapi.secret.gateway.SecretService
+import com.br.mysecretapi.secret.gateway.io.SecretInput
+import com.br.mysecretapi.secret.gateway.io.SecretOutput
+import com.br.mysecretapi.user.gateway.UserService
+import org.springframework.stereotype.Service
 
-class SecretServiceImpl : SecretService {
+@Service
+class SecretServiceImpl(
+    private var repository: SecretRepository,
+    private var userService: UserService) : SecretService {
+    override fun create(input: SecretInput) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByUser(userId: Long): List<SecretOutput> {
+        TODO("Not yet implemented")
+    }
 }
